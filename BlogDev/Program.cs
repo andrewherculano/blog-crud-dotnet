2d0a80ad92d57cda57230335cd06bcf9f76d254f
Andrew Herculano <andrewherculano@hotmail.com>
2022-05-13 15:15:41 -0300

    add .gitignore

---
+++ BlogDev/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlogDev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
